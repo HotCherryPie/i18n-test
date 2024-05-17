4f2b702183bdf3c0de395bebc352efe4c64aeebc
Oscar Fall <name@example.com>
2024-05-17 16:31:08 +0300

    wip

--- src/hooks/useTranslations.ts
+++ src/hooks/useTranslations.ts
@@ -1,14 +1,16 @@
 import type { ArrayIndices } from 'type-fest';
 import type { Index } from '../translations-2';
 
+type Volume = Index[number][0];
+
 type Mapping = {
   [K in ArrayIndices<Index> as Index[K][0]]: Record<Index[K][1], string>;
 };
 
-export const useTranslations = () => {
-  return {} as Mapping;
+export const useTranslations = <TVolume extends Volume>(volume: TVolume) => {
+  return {} as Mapping[TVolume];
 };
 
-const x = useTranslations();
+const x = useTranslations('common');
 
-x.common.stringA;
+x.stringB;
